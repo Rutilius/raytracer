aea95317d1f67c60fadc539c3bede8325c825673
Rutilius <rutilius.imr@gmail.com>
2023-03-04 19:45:20 +0100

    Added normal color to the sphere

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <cmath>
 #include <iostream>
 #include <memory>
 #include "color.h"
@@ -9,20 +10,26 @@
 
 
 
-bool hit_shpere(const point3 &center, const double radius, const ray &r) {
+double hit_shpere(const point3 &center, const double radius, const ray &r) {
     vec3 oc = r.origin - center;
     auto a = dot(r.direction, r.direction);
     auto b = 2.0 * dot(oc, r.direction);
     auto c = dot(oc, oc) - radius*radius;
     auto discriminant = b*b - 4*a*c;
-    return discriminant > 0;
+    if(discriminant < 0) {
+        return -1;
+    } else {
+        return (-b - sqrt(discriminant)) / (2.0 * a);
+    }
 }
 
 color ray_color(const ray &r) {
-    if(hit_shpere(vec3(0, 0, -1.0), 0.5, r)) {
-        return color(1, 0, 0);
+    auto t = hit_shpere(vec3(0, 0, -1.0), 0.5, r);
+    if(t > 0.0) {
+        vec3 N = normalized(r.at(t) - vec3(0, 0, -1));
+        return 0.5*color(N.x + 1, N.y + 1, N.z + 1);
     }
-    auto t = 0.5 * (r.direction.y + 1.0);
+    t = 0.5 * (r.direction.y + 1.0);
     return (1.0 - t)*color(1.0) + t*color(0.5, 0.7, 1.0);
 }
 
