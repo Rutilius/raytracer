6136dace560bb493c077dd66897d6743fd2098ac
Rutilius <rutilius.imr@gmail.com>
2023-03-06 11:56:42 +0100

    Added comment in write_color function

--- include/color.h
+++ include/color.h
@@ -8,11 +8,13 @@ constexpr void write_color(char img[], int index, const color &pixel_color, int 
     auto g = pixel_color.y;
     auto b = pixel_color.z;
 
+    // Divide the color by the number of samples.
     auto scale = 1.0 / sample_per_pixel;
     r *= scale;
     g *= scale;
     b *= scale;
 
+    // Write the translated [0,255] value of each color component.
     img[index    ] = static_cast<char>(256 * clamp(r, 0.0, 0.999));
     img[index + 1] = static_cast<char>(256 * clamp(g, 0.0, 0.999));
     img[index + 2] = static_cast<char>(256 * clamp(b, 0.0, 0.999));
