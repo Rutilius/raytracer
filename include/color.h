d864fb4449613db3fe16c67c84a41898b3c32fdc
Rutilius <rutilius.imr@gmail.com>
2023-03-06 12:03:36 +0100

    Added gamma correction

--- include/color.h
+++ include/color.h
@@ -8,11 +8,11 @@ constexpr void write_color(char img[], int index, const color &pixel_color, int 
     auto g = pixel_color.y;
     auto b = pixel_color.z;
 
-    // Divide the color by the number of samples.
+    // Divide the color by the number of samples and gamma-correct for gamma=2.0.
     auto scale = 1.0 / sample_per_pixel;
-    r *= scale;
-    g *= scale;
-    b *= scale;
+    r = sqrt(scale * r);
+    g = sqrt(scale * g);
+    b = sqrt(scale * b);
 
     // Write the translated [0,255] value of each color component.
     img[index    ] = static_cast<char>(256 * clamp(r, 0.0, 0.999));
