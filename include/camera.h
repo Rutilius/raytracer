fcd7834a00d46eb8c51eee4dd45ae1e24b1f1081
Rutilius <rutilius.imr@gmail.com>
2023-03-07 14:35:39 +0100

    Added movable camera

--- include/camera.h
+++ include/camera.h
@@ -2,19 +2,28 @@
 
 #include "rtweekend.h"
 
+
 class camera {
     public:
-        camera(const double vfov, const double aspect_ratio) {
+        camera(
+                const point3 lookfrom, const point3 lookat, const vec3 vup,
+                const double vfov, // vertical field-of-view in degrees
+                const double aspect_ratio
+            ) {
+
             const auto theta = degree_to_radians(vfov);
             const auto h = tan(theta / 2.0); 
             const auto viewport_height = 2.0 * h;
             const auto viewport_width = viewport_height * aspect_ratio;
-            const auto focal_length = 1.0;
 
-            origin = point3(0.0);
-            horizontal = vec3(viewport_width, 0.0, 0.0);
-            vertical = vec3(0.0, viewport_height, 0.0);
-            lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - vec3(0.0, 0.0, focal_length);
+            const auto w = normalized(lookfrom - lookat);
+            const auto u = normalized(cross(vup, w));
+            const auto v = cross(w, u);
+
+            origin = lookfrom;
+            horizontal = viewport_width * u;
+            vertical = viewport_height * v;
+            lower_left_corner = origin - horizontal/2.0 - vertical/2.0 - w;
         }
 
         ray get_ray(double u, double v) const {
