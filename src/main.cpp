643b091a238af3d8f2541376ebeceab1f4630921
Rutilius <rutilius.imr@gmail.com>
2023-03-13 16:43:55 +0100

    WIP Adding thread rendering

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,16 @@
 #include "hittable.h"
 #include "rtweekend.h"
 
+#include <cmath>
+#include <cstdlib>
+#include <functional>
 #include <iostream>
 #include <memory>
+#include <string>
+#include <thread>
+#include <vector>
+#include <time.h>   
+
 #include "hittable_list.h"
 #include "sphere.h"
 #include "vec3.h"
@@ -79,36 +87,147 @@ hittable_list random_scene() {
     return world;
 }
 
+struct render_data {
+    uint width;
+    uint height;
+
+    uint sample_per_pixel;
+    uint max_depth;
+
+    camera cam;
+    hittable_list world;
+
+    render_data(const uint width, const uint height, const uint sample_per_pixel, const uint max_depth, const camera &camera, const hittable_list &world) : 
+        width(width),
+        height(height),
+        sample_per_pixel(sample_per_pixel),
+        max_depth(max_depth),
+        cam(camera),
+        world(world)
+    { }
+};
+
+struct render_chunk {
+    int start_x;
+    int end_x;
+
+    int start_y;
+    int end_y;
+
+    render_chunk(uint start_x, uint end_x, uint start_y, uint end_y) : start_x(start_x), end_x(end_x), start_y(start_y), end_y(end_y) {}
+};
+
+void render(char img[], const int channels, const render_chunk &chunk, const render_data &data) {
+    for(int j = chunk.end_y - 1; j >= chunk.start_y; --j) {
+        for (int i = chunk.start_x; i < chunk.end_x; ++i) {
+            color pixel_color(0);
+            
+            for (uint s = 0; s < data.sample_per_pixel; ++s) {
+                auto u = (i + random_double()) / (data.width - 1);
+                auto v = (j + random_double()) / (data.height - 1);
+                ray r = data.cam.get_ray(u, v);
+                pixel_color += ray_color(r, data.world, data.max_depth);
+            }
+
+            auto start_color_index((j * channels * data.width) + i*channels);
+            write_color(img, start_color_index, pixel_color, data.sample_per_pixel);
+        }
+        std::cout << "Rendered line: " << j << ", column: [" << chunk.start_x << ", " << chunk.end_x << "]\n";
+    }
+}
+
+// Copyright (c) 2011 Bonelli Nicola <bonelli@antifork.org> (GPL v2 licensed)
+static unsigned int hardware_concurrency() {
+    auto proc = []() -> int {
+        std::ifstream cpuinfo("/proc/cpuinfo");
+        return std::count(std::istream_iterator<std::string>(cpuinfo),
+                        std::istream_iterator<std::string>(),
+                        std::string("processor"));
+    };
+
+    auto hc = std::thread::hardware_concurrency();
+    return hc ? hc : proc();
+}
+
 int main(int, char**) {
+    srand(time(NULL));
     const char *result_path = "result.png";
 
     // Image
     constexpr auto aspect_ratio = 3.0 / 2.0;
     constexpr int image_width = 1200;
     constexpr int image_height = static_cast<int>(image_width / aspect_ratio);
-    constexpr int sample_per_pixel = 500;
+    constexpr int sample_per_pixel = 100;
     constexpr int max_depth = 50;
 
     // World
-    auto world = random_scene();
+    const auto world = random_scene();
 
     // Camera
-    point3 lookfrom(13.0, 2.0, 3.0);
-    point3 lookat(0.0);
-    vec3 vup(0.0, 1.0, 0.0);
-    auto depth_of_field = 10.0;
-    auto aperture = 0.1;
+    constexpr point3 lookfrom(13.0, 2.0, 3.0);
+    constexpr point3 lookat(0.0);
+    constexpr vec3 vup(0.0, 1.0, 0.0);
+    constexpr auto depth_of_field = 10.0;
+    constexpr auto aperture = 0.1;
 
-    camera cam(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, depth_of_field);
+    const camera cam(lookfrom, lookat, vup, 20.0, aspect_ratio, aperture, depth_of_field);
 
     // Render
-    constexpr int channels = 3;
-    stbi_flip_vertically_on_write(true);
-    std::unique_ptr<char[]> img = std::make_unique<char[]>(image_width * image_height * channels);
+    const auto thread_count = hardware_concurrency();
+    std::cout << "Number of thread: " << thread_count << "\n";
 
-    for(int j = image_height - 1; j >= 0 ; --j) {
+    constexpr int channels = 3;
+    constexpr int pixels_channels_quantity = image_width * image_height * channels;
+    stbi_flip_vertically_on_write(true);
+    auto img = std::make_unique<char[]>(pixels_channels_quantity);
+
+    
+
+    if(thread_count > 0) {
+        // Multithread approach
+        auto threads = std::make_unique<std::thread[]>(thread_count);
+        auto img_variations = std::make_unique<std::unique_ptr<char[]>[]>(thread_count);
+        const render_data data(image_width, image_height, sample_per_pixel / thread_count, max_depth, cam, world);
+            
+        for (uint i = 0; i < thread_count; ++i) {
+            img_variations[i] = std::make_unique<char[]>(pixels_channels_quantity);
+            threads[i] = std::thread(render, img_variations[i].get(), channels, render_chunk(0, image_width, 0, image_height), std::ref(data));
+        }
+
+        for (uint i = 0; i < thread_count; ++i) {
+            threads[i].join();
+        }
+
+        // Save intermediate
+        for (uint i = 0; i < thread_count; ++i) {
+            char filename[19] = "intermediate__.png";// + "1" + ".png";
+            switch (i) {
+                case 0: filename[13] = '1'; break; 
+                case 1: filename[13] = '2'; break; 
+                case 2: filename[13] = '3'; break; 
+                case 3: filename[13] = '4'; break; 
+            }
+            stbi_write_png((const char *)filename, image_width, image_height, channels, img_variations[i].get(), image_width * channels);
+        }
+
+        for (int i = 0; i < pixels_channels_quantity; ++i) {
+            double channel_value = 0.0;
+            for (uint j = 0; j < thread_count; ++j) {
+                channel_value += img_variations[j][i];
+            }
+            img[i] = static_cast<char>(clamp(channel_value / thread_count, 0.0, 256.0));
+        }
+
+    } else {
+        // Singlethread approach
+        const render_data data(image_width, image_height, sample_per_pixel, max_depth, cam, world);
+        render(img.get(), channels, render_chunk(0, image_width, 0, image_height), data);
+    }
+
+    /*
+    for(uint j = image_height - 1; j >= 0 ; --j) {
         std::cout << "\rScanlines remaining: " << j << std::flush;
-        for (int i = 0; i < image_width; ++i) {
+        for (uint i = 0; i < image_width; ++i) {
             color pixel_color(0);
             
             for (int s = 0; s < sample_per_pixel; ++s) {
@@ -122,6 +241,7 @@ int main(int, char**) {
             write_color(img.get(), start_color_index, pixel_color, sample_per_pixel);
         }
     }
+    */
 
     stbi_write_png(result_path, image_width, image_height, channels, img.get(), image_width * channels);
     
